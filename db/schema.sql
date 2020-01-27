6a93a8fbd3914c212e4295b34fe0793117ff9611
briggySmalls <briggySmalls90@gmail.com>
2020-01-27 01:42:48 +0000

    Create database for testing

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+#
+# Create tables
+#
+
+# User accounts
+CREATE TABLE `accounts` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(128) NOT NULL,
+  `password` VARCHAR(128) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+# Devices
+CREATE TABLE `devices` (
+  `id` BINARY(16) NOT NULL,
+  `account_id` INT(11)
+  PRIMARY KEY (id),
+  FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+# Emails
+CREATE TABLE `emails` (
+  `id` INT(11),
+  `email` VARCHAR(128) NOT NULL,
+  `account_id` INT(11) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
