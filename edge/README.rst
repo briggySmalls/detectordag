8aae5637cf9ad0aa90284de1fa58312d0b9d893c
briggySmalls <briggySmalls90@gmail.com>
2020-02-12 00:21:42 +0000

    Commit cookiecutter template output

---
+++ edge/README.rst
@@ -0,0 +1,37 @@
+================
+detectordag edge
+================
+
+
+.. image:: https://img.shields.io/pypi/v/detectordag_edge.svg
+        :target: https://pypi.python.org/pypi/detectordag_edge
+
+.. image:: https://img.shields.io/travis/briggySmalls/detectordag_edge.svg
+        :target: https://travis-ci.org/briggySmalls/detectordag_edge
+
+.. image:: https://readthedocs.org/projects/detectordag-edge/badge/?version=latest
+        :target: https://detectordag-edge.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+
+Software for edge device in detectordag system
+
+
+* Free software: MIT license
+* Documentation: https://detectordag-edge.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ edge/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Sam Briggs",
+    author_email='briggySmalls90@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Software for edge device in detectordag system",
+    entry_points={
+        'console_scripts': [
+            'detectordag_edge=detectordag_edge.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='detectordag_edge',
+    name='detectordag_edge',
+    packages=find_packages(include=['detectordag_edge']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/briggySmalls/detectordag_edge',
+    version='0.1.0',
+    zip_safe=False,
+)
