180ff2ea19b303b5119347ca35e03d5a35733429
briggySmalls <briggySmalls90@gmail.com>
2020-02-15 11:32:06 +0000

    Add new client

---
+++ edge/edge/aws.py
@@ -0,0 +1,57 @@
+"""Logic for connecting to AWS IoT"""
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+logger = logging.getLogger(__file__)
+
+
+@dataclass
+class ClientConfig:
+    device_id: str
+    root_cert: Path
+    thing_cert: Path
+    thing_key: Path
+    endpoint: str
+    port: int
+    draining_frequency: int = 2
+    disconnect_timeout: int = 10
+    operation_timeout: int = 5
+
+
+class CloudClient:
+    def __init__(self, config: ClientConfig):
+        self.config = config
+        # Unique ID. If another connection using the same key is opened the
+        # previous one is auto closed by AWS IOT
+        self.client = AWSIoTMQTTClient(config.device_id)
+        # Used to configure the host name and port number the underneath AWS
+        # IoT MQTT Client tries to connect to.
+        self.client.configureEndpoint(self.config.endpoint, self.config.port)
+        # Used to configure the rootCA, private key and certificate files.
+        # configureCredentials(CAFilePath, KeyPath='', CertificatePath='')
+        self.client.configureCredentials(str(self.config.root_cert.resolve()),
+                                         str(self.config.thing_key.resolve()),
+                                         str(self.config.thing_cert.resolve()))
+        # Configure the offline queue for publish requests to be 20 in size and
+        # drop the oldest
+        self.client.configureOfflinePublishQueueing(-1)
+        # Used to configure the draining speed to clear up the queued requests
+        # when the connection is back. (frequencyInHz)
+        self.client.configureDrainingFrequency(self.config.draining_frequency)
+        # Configure connect/disconnect timeout to be 10 seconds
+        self.client.configureConnectDisconnectTimeout(
+            self.config.disconnect_timeout)
+        # Configure MQTT operation timeout to be 5 seconds
+        self.client.configureMQTTOperationTimeout(
+            self.config.operation_timeout)
+
+    def __enter__(self) -> 'CloudClient':
+        # Connect
+        self.client.connect(self.config.endpoint, self.config.port)
+
+    def __exit__(self, type, value, traceback) -> None:
+        del type, value, traceback
+        self.client.disconnect()
