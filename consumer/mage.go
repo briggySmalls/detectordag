7bac2d0e32594bcc118ad3c37b4ef493d2d8853b
briggySmalls <briggySmalls90@gmail.com>
2020-03-07 22:29:46 +0000

    Remove unnecessary stuff from magefile

--- consumer/mage.go
+++ consumer/mage.go
@@ -5,11 +5,11 @@ package main
 import (
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"io/ioutil"
+	"regexp"
 )
 
 type Build mg.Namespace
-
-var extraFlags string
 
 // Runs dep ensure and then installs the binary.
 func (Build) Production() error {
@@ -30,4 +30,25 @@ func (Build) Debug() error {
 
 func Delve() error {
 	return sh.Run("env", "GO111MODULE=off", "go", "build", "-o", "./delve/dlv", "$GOPATH/src/github.com/go-delve/delve/cmd/dlv")
+}
+
+func InstallTools() error {
+	// Read the tools file
+	toolsFile, err := ioutil.ReadFile("./tools/tools.go")
+	if err != nil {
+		return err
+	}
+
+	// Parse the tools
+	re := regexp.MustCompile(`_ \"(.*?)\"`)
+	matches := re.FindAllSubmatch(toolsFile, -1)
+
+	// Install
+	for _, match := range matches {
+		err := sh.Run("go", "install", string(match[1]))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
