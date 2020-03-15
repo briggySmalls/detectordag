13fd2fdf84331f3a33b3310bca12356083d310c5
briggySmalls <briggySmalls90@gmail.com>
2020-03-15 21:44:33 +0000

    Generate swagger source code

--- api/mage.go
+++ api/mage.go
@@ -4,15 +4,21 @@ package main
 
 import (
 	"github.com/briggysmalls/detectordag/shared"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
 )
 
 var helper shared.Lambda
+
+const apiSpecFile = "api.yaml"
 
 func init() {
 	helper = shared.New(".aws-sam/build/", "./tools/tools.go")
 }
 
-// Invokes the lambda function locally
+type Swagger mg.Namespace
+
+// Starts the API locally
 func StartApi() error {
 	return helper.StartApi()
 }
@@ -28,4 +34,12 @@ func Delve() error {
 
 func InstallTools() error {
 	return helper.InstallTools()
+}
+
+func (Swagger) Validate() error {
+	return sh.Run("swagger", "validate", apiSpecFile)
+}
+
+func (Swagger) Generate() error {
+	return sh.Run("swagger-codegen", "generate", "-i", "api.yaml", "--lang", "go-server")
 }
