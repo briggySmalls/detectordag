6b127c62fc783824ee754497cf3e2aa9c808d7f1
briggySmalls <briggySmalls90@gmail.com>
2020-03-17 21:17:48 +0000

    Refactor test to separate out reusable part

--- api/go/api_authentication_test.go
+++ api/go/api_authentication_test.go
@@ -2,6 +2,7 @@ package swagger
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/briggysmalls/detectordag/api/mocks"
 	"github.com/briggysmalls/detectordag/shared/database"
 	"github.com/dgrijalva/jwt-go"
@@ -12,40 +13,19 @@ import (
 	"testing"
 )
 
+const (
+	jwtSecret = "mysecret"
+)
+
 func TestAuthSuccess(t *testing.T) {
 	// Create some test constants
 	const (
 		username    = "email@example.com"
 		accountId   = "35581BF4-32C8-4908-8377-2E6A021D3D2B"
-		jwtSecret   = "mysecret"
 		jwtDuration = "2h"
 	)
-	// Create mock controller
-	ctrl := gomock.NewController(t)
-	// Create mock database client
-	c := mocks.NewMockClient(ctrl)
-	// Create unit under test
-	s := server{
-		db:     c,
-		config: Config{JwtSecret: jwtSecret, JwtDuration: jwtDuration},
-	}
-	// Configure the mock db client to expect a call to fetch the account
-	account := database.Account{
-		AccountId: accountId,
-		Username:  username,
-		Password:  "$2y$12$Nt3ajpggM4ViynWVGLOpW.JSbnVVVKRjNuw/ZYI71cj1WNG3Fty0K",
-	}
-	c.EXPECT().GetAccountByUsername(gomock.Eq(username)).Return(&account, nil)
-	// Create a request to authenticate
-	const body = `{"username": "email@example.com", "password": "mypassword"}`
-	req, err := http.NewRequest("POST", "/v1/auth", strings.NewReader(body))
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Run the handler using test code
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(s.Auth)
-	handler.ServeHTTP(rr, req)
+	// Run the test
+	rr := runTest(t, username, accountId, jwtDuration, "mypassword", "$2y$12$Nt3ajpggM4ViynWVGLOpW.JSbnVVVKRjNuw/ZYI71cj1WNG3Fty0K")
 	// Assert the HTTP status
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
@@ -53,6 +33,7 @@ func TestAuthSuccess(t *testing.T) {
 	}
 	// Check the response body is what we expect.
 	var resp Token
+	var err error
 	err = json.Unmarshal(rr.Body.Bytes(), &resp)
 	if err != nil {
 		t.Fatalf("Body could not be unmarshalled as a token: %v", rr.Body.String())
@@ -72,4 +53,34 @@ func TestAuthSuccess(t *testing.T) {
 	} else {
 		t.Fatalf(err.Error())
 	}
+}
+
+func runTest(t *testing.T, username, accountId, jwtDuration, password, hashedPassword string) *httptest.ResponseRecorder {
+	// Create mock controller
+	ctrl := gomock.NewController(t)
+	// Create mock database client
+	c := mocks.NewMockClient(ctrl)
+	// Create unit under test
+	s := server{
+		db:     c,
+		config: Config{JwtSecret: jwtSecret, JwtDuration: jwtDuration},
+	}
+	// Configure the mock db client to expect a call to fetch the account
+	account := database.Account{
+		AccountId: accountId,
+		Username:  username,
+		Password:  hashedPassword,
+	}
+	c.EXPECT().GetAccountByUsername(gomock.Eq(username)).Return(&account, nil)
+	// Create a request to authenticate
+	body := fmt.Sprintf(`{"username": "email@example.com", "password": "%s"}`, password)
+	req, err := http.NewRequest("POST", "/v1/auth", strings.NewReader(body))
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Run the handler using test code
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(s.Auth)
+	handler.ServeHTTP(rr, req)
+	return rr
 }
