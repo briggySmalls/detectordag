0c9d7afe10ea5905cdad1654c4f98c53e4467639
briggySmalls <briggySmalls90@gmail.com>
2020-03-25 23:35:37 +0000

    Add basic login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="login">
     <h1>This is the login page</h1>
+    <form
+        @submit="submit"
+        action="https://vuejs.org/"
+        method="post">
+      <div>
+        <label for="email">Email</label>
+        <input id="email" type="email" name="email" v-model="email" required>
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" v-model="password" required>
+      </div>
+      <input type="submit" value="Submit">
+    </form>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Login',
-};
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  private email = '';
+
+  private password = '';
+
+  public submit(event: Event) { // eslint-disable-line class-methods-use-this
+    // Submit a request to the backend
+    console.log(`Submitting request, {"email": "${this.email}", "password": "${this.password}"}`);
+    // Do not actually perform a post action
+    event.preventDefault();
+  }
+}
 </script>
