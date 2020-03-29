1de64706e4f58b02963c9e737758728e1e18e436
briggySmalls <briggySmalls90@gmail.com>
2020-03-29 19:15:00 +0100

    Move Content-Type response to middleware

--- api/swagger/api_devices.go
+++ api/swagger/api_devices.go
@@ -14,6 +14,5 @@ import (
 )
 
 func (s *server) UpdateDevice(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusNotImplemented)
 }
