7ef38fd0ba9dc5ef2c3851b7a1ca9cf540df2a5d
briggySmalls <briggySmalls90@gmail.com>
2020-03-30 13:15:17 +0100

    Move navbar to a component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-navbar toggleable="lg">
+    <!-- Logo -->
+    <b-navbar-brand href="#">
+      <img
+        id="logo" alt="DetectorDag logo" src="../assets/logo.svg"
+        class="d-inline-block">
+        DetectorDag
+    </b-navbar-brand>
+    <!-- Navbar -->
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <!-- Logged in navbar content -->
+        <template v-if="$store.state.account">
+          <b-nav-text>{{ username }}</b-nav-text>
+          <b-nav-item>
+            <b-button size="sm" v-on:click="logout">Logout</b-button>
+          </b-nav-item>
+        </template>
+        <!-- Loading -->
+        <template v-else>
+          <b-spinner></b-spinner>
+        </template>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Navbar extends Vue {
+  private get username() {
+    const { account } = this.$store.state;
+    return (account) ? account.username : '?';
+  }
+}
+</script>
--- frontend/src/views/Review.vue
+++ frontend/src/views/Review.vue
@@ -1,31 +1,7 @@
 <template>
   <div class="review">
-    <b-navbar toggleable="lg">
-      <!-- Logo -->
-      <b-navbar-brand href="#">
-        <img
-          id="logo" alt="DetectorDag logo" src="../assets/logo.svg"
-          class="d-inline-block">
-          DetectorDag
-      </b-navbar-brand>
-      <!-- Navbar -->
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <!-- Logged in navbar content -->
-          <template v-if="$store.state.account">
-            <b-nav-text>{{ username }}</b-nav-text>
-            <b-nav-item>
-              <b-button size="sm" v-on:click="logout">Logout</b-button>
-            </b-nav-item>
-          </template>
-          <!-- Loading -->
-          <template v-else>
-            <b-spinner></b-spinner>
-          </template>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <!-- Navbar -->
+    <NavbarComponent />
     <!-- Main page -->
     <h1>Review Dags</h1>
     <b-button
@@ -54,12 +30,14 @@ import { Component, Vue } from 'vue-property-decorator';
 import { Device } from '../../lib/client';
 import { storage } from '../utils';
 import DeviceComponent from '../components/Device.vue';
+import NavbarComponent from '../components/Navbar.vue';
 import ErrorComponent from '../components/Error.vue';
 
 @Component({
   components: {
     DeviceComponent,
     ErrorComponent,
+    NavbarComponent,
   },
 })
 export default class Review extends Vue {
@@ -113,11 +91,6 @@ export default class Review extends Vue {
     // Display the requested devices
     this.devices = data;
     this.isRefreshing = false;
-  }
-
-  private get username() {
-    const { account } = this.$store.state;
-    return (account) ? account.username : '?';
   }
 }
 </script>
