26d9f6c13bcd2c98c123e435fd1d5b49d26b525b
briggySmalls <briggySmalls90@gmail.com>
2020-03-30 16:18:43 +0100

    Add navigation to accounts route

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -10,6 +10,10 @@
     <!-- Navbar -->
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/review">Review</b-nav-item>
+        <b-nav-item to="/account">Account</b-nav-item>
+      </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <!-- Logged in navbar content -->
         <template v-if="$store.state.account">
