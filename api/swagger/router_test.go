56ab7dd5d1394c7373fee33872eec47335c08ddc
briggySmalls <briggySmalls90@gmail.com>
2020-04-20 23:44:09 +0100

    Add checks for options routes

--- api/swagger/router_test.go
+++ api/swagger/router_test.go
@@ -12,15 +12,13 @@ import (
 
 type expectFunc func(*MockServer)
 
-type testParams struct {
-	method     string
-	route      string
-	expectFunc expectFunc
-}
-
 func TestValidRoutes(t *testing.T) {
 	// Create requests to check
-	tps := []testParams{
+	tps := []struct {
+		method     string
+		route      string
+		expectFunc expectFunc
+	}{
 		{method: http.MethodPost, route: "/v1/auth", expectFunc: func(s *MockServer) {
 			s.EXPECT().Auth(gomock.Any(), gomock.Any()).Do(setStatusOk)
 		}},
@@ -37,7 +35,7 @@ func TestValidRoutes(t *testing.T) {
 			s.EXPECT().UpdateDevice(gomock.Any(), gomock.Any()).Do(setStatusOk)
 		}},
 	}
-
+	// Run the test iterations
 	for _, params := range tps {
 		// Create a request
 		r, err := http.NewRequest(params.method, params.route, nil)
@@ -53,13 +51,42 @@ func TestValidRoutes(t *testing.T) {
 	}
 }
 
+func TestOptionsRoutes(t *testing.T) {
+	// Create requests to check
+	tps := []struct {
+		route string
+	}{
+		{route: "/v1/auth"},
+		{route: "/v1/accounts/33b782d3-a2c8-40be-8aef-db5b44119bd5"},
+		{route: "/v1/accounts/cfe7d5ed-826e-4e31-bb46-d62aa1cb58a7"},
+		{route: "/v1/accounts/f88948e6-5f93-4f11-8d58-15d48075069d/devices"},
+		{route: "/v1/devices/c0e94a1b-a835-4cc2-9574-642bea13805a"},
+	}
+	// Run the test iterations
+	for _, params := range tps {
+		// Create a request
+		r, err := http.NewRequest(http.MethodOptions, params.route, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Run the request
+		w := runTest(t, r, nil)
+		// Ensure we get a 200
+		if s := w.Code; s != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %d", s)
+		}
+	}
+}
+
 func runTest(t *testing.T, r *http.Request, expect expectFunc) (w *httptest.ResponseRecorder) {
 	// Create router
 	server, router := createTestRouter(t)
 	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
 	w = httptest.NewRecorder()
 	// Configure the expectations
-	expect(server)
+	if expect != nil {
+		expect(server)
+	}
 	// Get the router to handle the request
 	router.ServeHTTP(w, r)
 	// Return the response for checking
