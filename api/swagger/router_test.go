31c0af439544e2f3288aa2a8c28ec7760eb0f54b
briggySmalls <briggySmalls90@gmail.com>
2020-04-20 23:27:03 +0100

    Test all valid routes

--- api/swagger/router_test.go
+++ api/swagger/router_test.go
@@ -22,6 +22,18 @@ func TestValidRoutes(t *testing.T) {
 		{method: http.MethodPost, route: "/v1/auth", expectFunc: func(s *MockServer) {
 			s.EXPECT().Auth(gomock.Any(), gomock.Any()).Do(setStatusOk)
 		}},
+		{method: http.MethodGet, route: "/v1/accounts/33b782d3-a2c8-40be-8aef-db5b44119bd5", expectFunc: func(s *MockServer) {
+			s.EXPECT().GetAccount(gomock.Any(), gomock.Any()).Do(setStatusOk)
+		}},
+		{method: http.MethodGet, route: "/v1/accounts/f88948e6-5f93-4f11-8d58-15d48075069d/devices", expectFunc: func(s *MockServer) {
+			s.EXPECT().GetDevices(gomock.Any(), gomock.Any()).Do(setStatusOk)
+		}},
+		{method: http.MethodPatch, route: "/v1/accounts/cfe7d5ed-826e-4e31-bb46-d62aa1cb58a7", expectFunc: func(s *MockServer) {
+			s.EXPECT().UpdateAccount(gomock.Any(), gomock.Any()).Do(setStatusOk)
+		}},
+		{method: http.MethodPatch, route: "/v1/devices/c0e94a1b-a835-4cc2-9574-642bea13805a", expectFunc: func(s *MockServer) {
+			s.EXPECT().UpdateDevice(gomock.Any(), gomock.Any()).Do(setStatusOk)
+		}},
 	}
 
 	for _, params := range testParams {
