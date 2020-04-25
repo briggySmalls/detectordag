14178cb48ef734c4ec6da4094d7ea78309838a73
briggySmalls <briggySmalls90@gmail.com>
2020-04-25 19:01:24 +0100

    Get a messy implementation requesting API

--- edge/access-point/pages/api/register.ts
+++ edge/access-point/pages/api/register.ts
@@ -1,11 +1,58 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { ApiClient, AccountsApi, AuthenticationApi, Credentials, Token, DeviceRegistered } from '../../lib/client';
+import util from 'util';
+
+
+// Define a wrapper for the different clients
+class ClientWrapper {
+  public readonly accounts: AccountsApi;
+
+  public readonly authentication: AuthenticationApi;
+
+  public constructor(client: ApiClient) {
+    this.accounts = new AccountsApi(client);
+    this.authentication = new AuthenticationApi(client);
+  }
+}
+
+// Create the underlying client
+const client = new ApiClient();
+client.basePath = `${process.env.API_BASEPATH}/v1`;
+
+// Create an instance of our wrapper
+const wrapper = new ClientWrapper(client);
 
 // Handle form submission
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  // Print the content
-  console.log(req.body);
-  // Assemble the response
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ name: 'John Doe' }))
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Pull out the results
+  const formData = req.body;
+  // Authenticate
+  try {
+    let result = await util.promisify(wrapper.authentication.auth.bind(wrapper.authentication))(new Credentials(formData.username, formData.password));
+    const token = JSON.parse(result.text);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).json({error: err});
+  }
+  // let result = await client.accounts.registerDevice(new , `Bearer ${auth.token}`, , auth.accountId);
+  // const registered = JSON.parse(result.text);
+
+  // // Create an async executor
+  // const run = async () => {
+  //   // First authorise
+  //   // Now save the credentials to file
+  //   console.log(token);
+  // };
+
+  // Run the executor
+  // const promise = new Promise(run).then((result) => {
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.status(200).json(result);
+  // }).catch((error) => {
+  //   res.setHeader('Content-Type', 'application/json');
+  //   res.status(500).json(error);
+  // });
 }
