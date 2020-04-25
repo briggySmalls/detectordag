39cf269cb97144d9398a0379f7c26feead785436
briggySmalls <briggySmalls90@gmail.com>
2020-04-25 08:42:18 +0100

    Remove some demo content

--- edge/access-point/pages/index.js
+++ edge/access-point/pages/index.js
@@ -19,46 +19,32 @@ export default function Home() {
           Register your device to get started
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
-        </div>
+        <form>
+          <TextField
+            id="username-input"
+            label="Username"
+            type="email"
+            autoComplete="current-password"
+            required
+            variant="outlined"
+          />
+          <TextField
+          id="password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          required
+          variant="outlined"
+          />
+          <TextField
+          id="device-name-input"
+          label="Desired device name"
+          type="text"
+          required
+          variant="outlined"
+          />
+        </form>
       </main>
-
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
