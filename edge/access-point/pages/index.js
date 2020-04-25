a9cc8f786e893c4e0bb7932b8453f94851660c5d
briggySmalls <briggySmalls90@gmail.com>
2020-04-25 09:36:10 +0100

    Add comment

--- edge/access-point/pages/index.js
+++ edge/access-point/pages/index.js
@@ -5,6 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+// Create some CSS styles for the page
 const useStyles = makeStyles((theme) => ({
   form: {
     display: 'flex',
