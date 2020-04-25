07dc4ccbbe496ddd053f7112414fbfbcec7032d7
briggySmalls <briggySmalls90@gmail.com>
2020-04-25 08:52:16 +0100

    Add roboto because I'm a pushover

---
+++ edge/access-point/pages/_app.js
@@ -0,0 +1,7 @@
+// Import roboto because material-ui likes it
+import 'typeface-roboto';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function DetectorDag({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
