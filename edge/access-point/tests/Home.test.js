3423d1b03dc04ca4a6481f9c4a3bfe79a5a5692f
briggySmalls <briggySmalls90@gmail.com>
2020-04-27 00:40:55 +0100

    Start testing axios calls

--- edge/access-point/tests/Home.test.js
+++ edge/access-point/tests/Home.test.js
@@ -1,15 +1,34 @@
 import { shallow } from "enzyme";
 import React from "react";
 import renderer from "react-test-renderer";
+import axios from 'axios';
 
 import Home from "../pages/index.js";
 import Layout from "../components/layout.js";
 
+// Mock axios for us
+jest.mock('axios');
+
 describe("With Enzyme", () => {
-  it('Home shows "Hello, Sunshine!"', () => {
+  it('Home has expected content', () => {
     const app = shallow(<Home />);
     expect(app.find("Layout")).toHaveLength(1);
+    expect(app.find("Layout Alert")).toHaveLength(0);
     expect(app.find("Layout p").text()).toEqual("Register your device to get started");
+  });
+
+  if('Home calls API', () => {
+    const app = shallow(<Home />);
+    // Simulate a submission
+    const testData = {test: 'data'};
+    app.find("Layout FormWithLoading").props.onSubmit(testData);
+    // Expect axios to have been called
+    const resp = {data: users};
+    axios.post.mockResolvedValue(resp);
+    // Assert
+    expect(axios.post.mock.calls.length).toBe(1);
+    expect(axios.post.mock.calls[0][0]).toBe('/api/register');
+    expect(axios.post.mock.calls[0][1]).toBe(testData);
   });
 });
 
