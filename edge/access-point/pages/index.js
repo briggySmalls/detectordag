e2bb593b332a72ba6c5805bf13327ab6cf35f3d9
briggySmalls <briggySmalls90@gmail.com>
2020-04-26 23:21:38 +0100

    Handle response codes

--- edge/access-point/pages/index.js
+++ edge/access-point/pages/index.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import WithLoading from '../components/WithLoading';
 import Layout from '../components/layout';
 import { makeStyles } from '@material-ui/core/styles';
+import { useRouter } from 'next/router'
+import Alert from '@material-ui/lab/Alert';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
@@ -21,6 +23,8 @@ const useStyles = makeStyles((theme) => ({
 const Form = ({ onSubmit }) => {
   // Create styles for use
   const classes = useStyles();
+  // Get the nextjs router
+  const router = useRouter()
   // Render the form
   return (
     <form className={classes.form} onSubmit={onSubmit}>
@@ -70,6 +74,8 @@ export default function Home() {
   const classes = useStyles();
   // Declare isLoading state
   const [isLoading, setIsLoading] = useState(false);
+  // Declare isLoading state
+  const [error, setError] = useState(null);
   // Callback for submit event
   const handleSubmit = (event) => {
     // Don't actually submit
@@ -78,12 +84,23 @@ export default function Home() {
     const formData = new FormData(event.target);
     let data = {};
     formData.forEach((value, key) => {data[key] = value});
+    // Clear any errors
+    setError(null);
     // Submit the form data
     axios({
       method: 'post',
       url: '/api/register',
       data: data,
-    })
+    }).then((response) => {
+      // Transition to the success page
+      router.push('/success');
+    }).catch((error) => {
+      // Show the error
+      setError(error);
+    }).then(() => {
+      // Indicate we've finished loading
+      setIsLoading(false);
+    });
     // Update the state
     setIsLoading(true);
   }
