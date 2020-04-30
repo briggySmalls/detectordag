7add31ef53619608a430de3d83683f881cb98c0b
briggySmalls <briggySmalls90@gmail.com>
2020-05-01 00:23:54 +0100

    Move deletion to fixture

--- edge/detector/tests/test_config.py
+++ edge/detector/tests/test_config.py
@@ -20,7 +20,7 @@ class EnvVarProps:
 
 
 @pytest.fixture
-def variables(tmp_path: Path) -> Dict[str, Any]:
+def variables(monkeypatch: Any, tmp_path: Path) -> Dict[str, Any]:
     # Establish the paths for the thing certificates
     certs_dir = tmp_path
     aws_thing_cert_path = certs_dir.joinpath('thing.cert.pem')
@@ -29,7 +29,7 @@ def variables(tmp_path: Path) -> Dict[str, Any]:
     copyfile(_TEST_DATA_DIR.joinpath('thing.cert.pem'), aws_thing_cert_path)
     copyfile(_TEST_DATA_DIR.joinpath('thing.private.key'), aws_thing_key_path)
     # Create the variables
-    return {
+    variables = {
         "AWS_ENDPOINT": "www.test.com",
         "BALENA_DEVICE_UUID": "03076b52-3a66-425b-ad2d-43e925486e60",
         "AWS_ROOT_CERT": "LS0tLS2CRUdJTiBDRVJUSUZJQ0FURS0tqS0tCk1JSURRVENDQWltZ0F3SUJBZ0lUQm15Zno1bS9qQW81NHZCNGlrUG1salpieWpBTkJna3Foa2lHOXcwQkFRc0YKQURBNU1Rc3dDUVlEVlFRR0V3SlZVekVQTUEwR0ExVUVDaE1HUVcxaGVtOXVNUmt3RndZRFZRUURFeEJCYldGNgpiMjRnVW05dmRDQkRRU0F4TUI0WERURTFNRFV5TmpBd01EQXdNRm9YRFRNNE1ERXhOekF3TURBd01Gb3dPVEVMCk1Ba0dBMVVFQmhNQ1ZWTXhEekFOQmdOVkJBb1RCa0Z0WVhwdmJqRVpNQmNHQTFVRUF4TVFRVzFoZW05dUlGSnYKYjNRZ1EwRWdNVENDQVNJd0RRWUpLb1pJaHZjTkFRRUJCUUFEZ2dFUEFEQ0NBUW9DZ2dFQkFMSjRnSEhLZU5YagpjYTlIZ0ZCMGZXN1kxNGgyOUpsbzkxZ2hZUGwwaEFFdnJBSXRodE9nUTNwT3NxVFFOcm9Cdm8zYlNNZ0hGelpNCjlPNklJOGMrNnpmMXRSbjRTV2l3M3RlNWRqZ2RZWjZrL29JMnBlVktWdVJGNGZuOXRCYjZkTnFjbXpVNUwvcXcKSUZBR2JIclFnTEttK2Evc1J4bVBVRGdIM0tLSE9WajR1dFdwK1Vobk1KYnVsSGhlYjRtalVjQXdobWFoUldhNgpWT3VqdzVINVNOei8wZWd3TFgwdGRIQTExNGdrOTU3RVdXNjdjNGNYOGpKR0tMaEQrcmNkcXNxMDhwOGtEaTFMCjkzRmNYbW4vNnBVQ3l6aUtybEE0Yjl2N0xXSWJ4Y2NlVk9GMzRHZklENXlISTlZL1FDQi9JSURFZ0V3K095UW0KamdTdWJKcklxZzBDQXdFQUFhTkNNRUF3RHdZRFZSMFRBUUgvQkFVd0F3RUIvekFPQmdOVkhROEJBZjhFQkFNQwpBWVl3SFFZRFZSME9CQllFRklRWXpJVTA3THdNbEpRdUNGbWN4N0lRVGdvSU1BMEdDU3FHU0liM0RRRUJDd1VBCkE0SUJBUUNZOGpkYVFaQ2hHc1YyVVNnZ05pTU9ydVlvdTZyNGxLNUlwREIvRy93a2pVdTB5S0dYOXJieGVuREkKVTVQTUNDamptQ1hQSTZUNTNpSFRmSVVKclU2YWRUckNDMnFKZUhaRVJ4aGxiSTFCamp0L21zdjB0YWRRMXdVcwpOK2dEUzYzcFlhQUNidlh5OE1XeTdWdTMzUHFVWEhlZUU2Vi9VcTJWOHZpVE85NkxYRnZLV2xKYllLOFU5MHZ2Cm8vdWZRSlZ0TVZUOFF0UEhSaDhqcmRrUFNIQ2EyWFY0Y2RGeVF6UjFibGRad2dKY0ptQXB6eU1aRm82SVE2WFUKNU1zSSt5TVJRK2hES1hKaW9hbGRYZ2pVa0s2NDJNNFV3dEJWOG9iMnhKTkRkMlpod0xub1FkZVhlR0FEYmtweQpycVhSZmJvUW5vWnNHNHE1V1RQNDY4U1F2dkc1Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K",  # noqa: E501 pylint: disable=line-too-long
@@ -38,6 +38,10 @@ def variables(tmp_path: Path) -> Dict[str, Any]:
         "AWS_PORT": 8080,
         "ALIVE_INTERVAL": 3600,
     }
+    # Delete existing environment variables
+    for key in variables.keys():
+        monkeypatch.delenv(key, raising=False)
+    return variables
 
 
 def test_present(monkeypatch: Any, variables: Dict[str, EnvVarProps]) -> None:
@@ -47,9 +51,6 @@ def test_present(monkeypatch: Any, variables: Dict[str, EnvVarProps]) -> None:
         monkeypatch (TYPE): Fixture for configuring environment
         tmp_path (TYPE): Fixture for supplying a temporary directory
     """
-    # Delete existing environment variables
-    for key in variables.keys():
-        monkeypatch.delenv(key, raising=False)
     # Configure the environment variables
     for name, value in variables.items():
         monkeypatch.setenv(name, value)
@@ -74,11 +75,6 @@ def test_present(monkeypatch: Any, variables: Dict[str, EnvVarProps]) -> None:
     "BALENA_DEVICE_UUID",
 ])
 def test_missing_env(monkeypatch: Any, variables: Dict[str, EnvVarProps], to_drop: str) -> None:
-    """Run a test, dropping each of the variables in turn"""
-    # Delete existing environment variables
-    for key in variables.keys():
-        monkeypatch.delenv(key, raising=False)
-    # Now set all variables, skipping variable under test
     for key, value in variables.items():
         if key != to_drop:
             # Set the variable in the environment
