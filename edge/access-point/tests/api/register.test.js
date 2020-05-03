5fc11408f5fba2e7bd43593bdbf813cd847b8839
briggySmalls <briggySmalls90@gmail.com>
2020-05-03 18:09:13 +0100

    Minor rewording

--- edge/access-point/tests/api/register.test.js
+++ edge/access-point/tests/api/register.test.js
@@ -5,7 +5,7 @@ import register from "../pages/api/register.js";
 // Mock axios for us
 jest.mock('client');
 
-describe("With Enzyme", () => {
+describe("Register API", () => {
   it('Walks the happy path', () => {
     // First expect a request to auth
     axios.post.mockResolvedValueOnce()
