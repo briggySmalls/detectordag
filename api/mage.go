6d522057d7e234779dfd687ab99d8fb0de2ccf54
briggySmalls <sam.briggs@entia.co>
2020-10-06 00:35:07 +0100

    Add ability to clean mocks

--- api/mage.go
+++ api/mage.go
@@ -7,4 +7,9 @@ import (
 	_ "github.com/briggysmalls/detectordag/shared/mage"
 	// mage:import
 	_ "github.com/briggysmalls/detectordag/shared/mage/api"
+	"github.com/magefile/mage/sh"
 )
+
+func Clean() error {
+	return sh.RunV("find", ".", "-name", "mock_*.go", "-delete")
+}
