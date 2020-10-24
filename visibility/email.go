139ce5b5e07ab5ecdce99eb8e2849025e92b066b
briggySmalls <briggySmalls90@gmail.com>
2020-10-24 13:06:29 +0100

    Fixup visibility in line with email client refactor

--- visibility/email.go
+++ visibility/email.go
@@ -1,144 +1,57 @@
 package visibility
 
 import (
-	"bytes"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/briggysmalls/detectordag/shared"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/briggysmalls/detectordag/shared/database"
 	"github.com/briggysmalls/detectordag/shared/email"
 	"github.com/briggysmalls/detectordag/shared/iot"
-	"html/template"
+
 	"log"
 	"time"
 )
 
-const (
-	// Address from which emails will be sent
-	Sender     = "briggySmalls90@gmail.com"
-	DateFormat = "15:04 2/1/06"
-)
-
-const textTemplateSource = `
-Visibility status update.
-
-Whilst keeping an eye on your dag, we noticed something change.
-Device: {{ .DeviceName }}
-Time: {{ .Timestamp.Format "15:04 02-Jan-2006" }}
-Status: {{ if .Status }}👋Found{{else}}💨Lost{{end}}
-
-{{ if .Status }}
-We're back in business!
-{{ else }}
-Maybe it's time to send a neighbour round?
-{{ end }}
-Sent with ❤ from a dag
-`
-
-const htmlTemplateSource = `
-<h1>Visibility status update</h1>
-<p>
-	Whilst keeping an eye on your dag, we noticed something change.
-</p>
-<table>
-  <tr>
-    <td>Device</td>
-    <td>{{ .DeviceName }}</td>
-  </tr>
-  <tr>
-    <td>Time</td>
-    <td>{{ .Timestamp.Format "15:04 02-Jan-2006" }}</td>
-  </tr>
-  <tr>
-    <td>Status</td>
-    <td>{{ if .Status }}👋Found{{else}}💨Lost{{end}}</td>
-  </tr>
-</table>
-{{ if .Status }}
-<p>
-	We're back in business!
-</p>
-{{ else }}
-<p>
-	Maybe it's time to send a neighbour round?
-</p>
-{{ end }}
-<p>
-	Sent with ❤ from a dag
-</p>
-`
-
-type VisibilityStatusChangedEmailConfig struct {
-	DeviceName string
-	Timestamp  time.Time
-	Status     bool
+type emailClient struct {
+	email email.Client
+	db    database.Client
 }
 
-var emailClient email.Client
-var htmlTemplate *template.Template
-var textTemplate *template.Template
+type EmailClient interface {
+	SendVisiblityStatus(device *iot.Device, timestamp time.Time, status bool) error
+}
 
-// init initialises a client for AWS SES
-func init() {
-	var err error
-	// Create a new session just for emailing (there is no emailing service in eu-west-2)
-	emailSesh := shared.CreateSession(aws.Config{Region: aws.String("eu-west-1")})
+func New(sesh *session.Session, db database.Client) (EmailClient, error) {
 	// Create a new email client
-	emailClient, err = email.New(emailSesh)
+	email, err := email.New(sesh, htmlTemplateSource, textTemplateSource)
 	if err != nil {
-		log.Fatal(err.Error())
+		return nil, err
 	}
-	// Create templates
-	htmlTemplate, err = template.New("htmlTemplate").Parse(htmlTemplateSource)
-	if err != nil {
-		log.Fatal(err)
-	}
-	textTemplate, err = template.New("textTemplate").Parse(textTemplateSource)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return &emailClient{email: email, db: db}, nil
 }
 
-func EmailVisiblityStatus(dbClient database.Client, device *iot.Device, timestamp time.Time, status bool) error {
+func (e *emailClient) SendVisiblityStatus(device *iot.Device, timestamp time.Time, status bool) error {
 	log.Printf("Sending visibility email for device: %s with state %v", DeviceString(device), status)
 	// Get the account
-	account, err := dbClient.GetAccountById(device.AccountId)
+	account, err := e.db.GetAccountById(device.AccountId)
 	if err != nil {
 		return err
 	}
-	// Notify the account owner their device is missing
-	return sendEmail(
-		account.Emails,
-		VisibilityStatusChangedEmailConfig{
-			DeviceName: device.Name,
-			Timestamp:  timestamp,
-			Status:     status,
-		},
-	)
-}
-
-func sendEmail(recipients []string, status VisibilityStatusChangedEmailConfig) error {
-	// Execute the templates
-	var err error
-	var htmlBody bytes.Buffer
-	err = htmlTemplate.Execute(&htmlBody, status)
-	if err != nil {
-		return err
+	// Assemble the visibility status context
+	context := struct {
+		DeviceName string
+		Timestamp  time.Time
+		Status     bool
+	}{
+		DeviceName: device.Name,
+		Timestamp:  timestamp,
+		Status:     status,
 	}
-	var textBody bytes.Buffer
-	err = textTemplate.Execute(&textBody, status)
-	if err != nil {
-		return err
-	}
+	// Determine the subject
 	var subject string
-	if status.Status {
+	if context.Status {
 		subject = "👋 We've found your dag again!"
 	} else {
 		subject = "💨 You're dag's gone missing!"
 	}
 	// Send the email.
-	err = emailClient.SendEmail(recipients, Sender, subject, htmlBody.String(), textBody.String())
-	if err != nil {
-		return err
-	}
-	return nil
+	return e.email.SendEmail(account.Emails, Sender, subject, context)
 }
