ab933c17c9b654394e8843553459e41247ced16b
briggySmalls <briggySmalls90@gmail.com>
2020-10-25 18:16:05 +0000

    Turn on logging for consumer

--- consumer/main.go
+++ consumer/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/briggysmalls/detectordag/consumer/app"
+	"log"
 )
+
+func init() {
+	// Add file/line number to the default logger
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
 
 // main is the entrypoint to the lambda function
 func main() {
