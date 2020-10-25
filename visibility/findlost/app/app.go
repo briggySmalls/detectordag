1fbf222fbb025a236ddf6f2cbf72f89e5c3fe9b1
briggySmalls <briggySmalls90@gmail.com>
2020-10-25 16:12:12 +0000

    Fix tests

--- visibility/findlost/app/app.go
+++ visibility/findlost/app/app.go
@@ -37,8 +37,6 @@ func New(
 
 // handleRequest handles a lambda call
 func (a *app) RunJob(ctx context.Context) error {
-	// Print out handler parameters
-	log.Print("Context: ", ctx)
 	// Request all devices that are considered 'visible'
 	devices, err := a.iot.GetThingsByVisibility(true)
 	if err != nil {
@@ -47,38 +45,46 @@ func (a *app) RunJob(ctx context.Context) error {
 	log.Printf("Checking %d devices for updated visibility status", len(devices))
 	// Iterate through devices
 	for _, device := range devices {
-		// Fetch the shadow
-		shdw, err := a.shadow.Get(device.DeviceId)
-		if err != nil {
-			return err
-		}
-		// Check we have a reported status
-		_, ok := shdw.State.Reported["status"].(bool)
-		if !ok {
-			return fmt.Errorf("%s doesn't have status", visibility.DeviceString(device))
-		}
-		// Check when the device was last seen
-		lastSeen := shdw.Metadata.Reported["status"].Timestamp.Time
-		if time.Now().Before(lastSeen.Add(a.lastSeenDuration)) {
-			// This device was seen recently enough
-			continue
-		}
-		if !device.Visibility {
-			// We searched for visible devices, something weird has happened
-			log.Printf("%s already marked lost despite searching for visible devices", visibility.DeviceString(device))
-			continue
-		}
-		// Mark the device as lost
-		err = a.iot.SetVisibiltyState(device.DeviceId, false)
-		if err != nil {
-			return err
-		}
-		// Email to say so
-		err = a.email.SendVisibilityStatus(device, lastSeen, false)
-		if err != nil {
-			return err
+		// Process all the devices, logging any errors
+		if err := a.processDevice(device); err != nil {
+			log.Print(err)
 		}
 	}
 	// Return the response
 	return err
+}
+
+func (a *app) processDevice(device *iot.Device) error {
+	// Fetch the shadow
+	shdw, err := a.shadow.Get(device.DeviceId)
+	if err != nil {
+		return err
+	}
+	// Check we have a reported status
+	_, ok := shdw.State.Reported["status"].(bool)
+	if !ok {
+		return fmt.Errorf("%s doesn't have status", visibility.DeviceString(device))
+	}
+	// Check when the device was last seen
+	lastSeen := shdw.Metadata.Reported["status"].Timestamp.Time
+	if time.Now().Before(lastSeen.Add(a.lastSeenDuration)) {
+		// This device was seen recently enough
+		return nil
+	}
+	if !device.Visibility {
+		// We searched for visible devices, something weird has happened
+		log.Printf("%s already marked lost despite searching for visible devices", visibility.DeviceString(device))
+		return nil
+	}
+	// Mark the device as lost
+	err = a.iot.SetVisibiltyState(device.DeviceId, false)
+	if err != nil {
+		return err
+	}
+	// Email to say so
+	err = a.email.SendVisibilityStatus(device, lastSeen, false)
+	if err != nil {
+		return err
+	}
+	return nil
 }
