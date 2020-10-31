2fb7ef33154a7a088c303b631a13e8a6d2c40e20
briggySmalls <briggySmalls90@gmail.com>
2020-10-31 01:23:42 +0000

    Add ReceiveMessage

--- shared/sqs/sqs_test.go
+++ shared/sqs/sqs_test.go
@@ -3,6 +3,7 @@ package sqs
 //go:generate mockgen -destination mock_sqs.go -package sqs github.com/aws/aws-sdk-go/service/sqs/sqsiface SQSAPI
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -27,6 +28,25 @@ func TestSend(t *testing.T) {
 		Connected: true,
 		Time:      time.Unix(0, 0).UTC(),
 	})
+}
+
+func TestReceive(t *testing.T) {
+	// Create the unit under test
+	client, isqs := createUnitAndMocks(t)
+	// Configure mock to expect a call
+	isqs.EXPECT().ReceiveMessage(gomock.Not(gomock.Nil())).Do(func(input *sqs.ReceiveMessageInput) {
+		assert.Equal(t, QueueUrl, *input.QueueUrl)
+		assert.Equal(t, int64(1), *input.MaxNumberOfMessages)
+	}).Return(&sqs.ReceiveMessageOutput{
+		Messages: []*sqs.Message{{Body: aws.String(`{"connected":true,"time":"1970-01-01T00:00:00Z"}`)}},
+	}, nil)
+	// Make the call
+	payload, err := client.ReceiveMessage()
+	assert.Nil(t, err)
+	assert.Equal(t, &ConnectionStatusPayload{
+		Connected: true,
+		Time:      time.Unix(0, 0).UTC(),
+	}, payload)
 }
 
 func createUnitAndMocks(t *testing.T) (Client, *MockSQSAPI) {
