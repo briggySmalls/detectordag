cec5033452afc14f68c7ff1995f394dd13c46b28
briggySmalls <briggySmalls90@gmail.com>
2020-10-31 00:29:07 +0000

    Create an sqs client

---
+++ shared/sqs/sqs.go
@@ -0,0 +1,53 @@
+package sqs
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
+)
+
+type client struct {
+	sqs      sqsiface.SQSAPI
+	queueUrl string
+}
+
+type ConnectionStatusPayload struct {
+	Connected bool `json:"connected"`
+}
+
+// Client is a client for sending status updates to the queue
+type Client interface {
+	SendMessage(payload ConnectionStatusPayload) error
+}
+
+// NewSender gets a new Client
+func New(sesh *session.Session, queueUrl string) (Client, error) {
+	// Create Amazon DynamoDB client
+	sqs := sqs.New(sesh)
+	if sqs == nil {
+		return nil, errors.New("Failed to create SQS client")
+	}
+	// Create our client wrapper
+	client := client{
+		sqs:      sqs,
+		queueUrl: queueUrl,
+	}
+	return &client, nil
+}
+
+func (c *client) SendMessage(payload ConnectionStatusPayload) error {
+	// Marshal the payload to a string
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	// Send the message
+	_, err = c.sqs.SendMessage(&sqs.SendMessageInput{
+		MessageBody: aws.String(string(body)),
+		QueueUrl:    aws.String(c.queueUrl),
+	})
+	return err
+}
---
+++ shared/sqs/sqs_test.go
@@ -0,0 +1,40 @@
+package sqs
+
+//go:generate mockgen -destination mock_sqs.go -package sqs github.com/aws/aws-sdk-go/service/sqs/sqsiface SQSAPI
+
+import (
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	QueueUrl = "myqueuename"
+)
+
+func TestSend(t *testing.T) {
+	// Create the unit under test
+	client, isqs := createUnitAndMocks(t)
+	// Configure mock to expect a call
+	isqs.EXPECT().SendMessage(gomock.Not(gomock.Nil())).Do(func(input *sqs.SendMessageInput) {
+		assert.Equal(t, `{"connected":true}`, *input.MessageBody)
+		assert.Equal(t, QueueUrl, *input.QueueUrl)
+	}).Return(nil, nil)
+	// Make the call
+	client.SendMessage(ConnectionStatusPayload{Connected: true})
+}
+
+func createUnitAndMocks(t *testing.T) (Client, *MockSQSAPI) {
+	// Create mock controller
+	ctrl := gomock.NewController(t)
+	// Create mock SQSAPI
+	mock := NewMockSQSAPI(ctrl)
+	// Create the unit under test
+	sender := client{
+		sqs:      mock,
+		queueUrl: QueueUrl,
+	}
+	// Create the new iot client
+	return &sender, mock
+}
