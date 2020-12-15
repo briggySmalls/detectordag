1c0174d0546df2f5e1bbdc3e0e23edfba6a796ff
briggySmalls <sam.briggs@entia.co>
2020-12-15 00:40:41 +0000

    Fix disconnected app tests

--- visibility/disconnected/app/app_test.go
+++ visibility/disconnected/app/app_test.go
@@ -7,10 +7,11 @@ package app
 import (
 	"errors"
 	"github.com/aws/aws-lambda-go/events"
-	// "github.com/briggysmalls/detectordag/shared/iot"
+	"github.com/briggysmalls/detectordag/shared/iot"
 	"github.com/briggysmalls/detectordag/shared/shadow"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"fmt"
 	"testing"
 	"time"
 )
@@ -75,59 +76,70 @@ func TestStaleEvent(t *testing.T) {
 	assert.Nil(t, app.Handler(nil, event))
 }
 
-// func TestDeviceLookupFailed(t *testing.T) {
-// 	// Create app under test
-// 	app, mockIoT, _ := getStubbedApp(t)
-// 	// Prepare some test parameters
-// 	const (
-// 		deviceID = "b6d62b30-00ac-49c4-9268-88559a46889f"
-// 	)
-// 	// Prepare an event
-// 	event := DeviceLifecycleEvent{
-// 		DeviceID:  deviceID,
-// 		Timestamp: 0,
-// 		EventType: "connected",
-// 	}
-// 	// Configure lookup to fail
-// 	mockIoT.EXPECT().GetThing(gomock.Eq(deviceID)).Return(nil, errors.New("Something went wrong"))
-// 	// Run test
-// 	assert.NotNil(t, app.RunJob(nil, event))
-// }
+func TestDeviceLookupFailed(t *testing.T) {
+	// Prepare some test parameters
+	const (
+		deviceID = "b6d62b30-00ac-49c4-9268-88559a46889f"
+	)
+	// Create app under test
+	app, mockIoT, mockShadow, _ := getStubbedApp(t)
+	// Prepare an event
+	// Construct the event
+	event := events.SQSEvent{
+		Records: []events.SQSMessage{
+			{Body: fmt.Sprintf(`{"deviceId":"%s","time":"2020-12-12T19:58:16+00:00"}`, deviceID)},
+		},
+	}
+	// Expect a call to shadow
+	mockShadow.EXPECT().GetConnectionStatus(deviceID).Return(
+		// Indicate the status hasn't been updated for a while
+		&shadow.ConnectionState{State: true, Updated: createTime(t, "2020/12/12 00:00:00")},
+		nil,
+	)
+	// Configure lookup to fail
+	mockIoT.EXPECT().GetThing(deviceID).Return(nil, errors.New("Something went wrong"))
+	// Run test
+	assert.NotNil(t, app.Handler(nil, event))
+}
 
-// func TestEmailsSent(t *testing.T) {
-// 	testParams := []struct {
-// 		event     string
-// 		status    bool
-// 		timestamp int64
-// 		time      time.Time
-// 	}{
-// 		{event: "connected", status: true, timestamp: 0, time: createTime(t, "1970/01/01 00:00:00")},
-// 		{event: "disconnected", status: false, timestamp: 0, time: createTime(t, "1970/01/01 00:00:00")},
-// 	}
-// 	const (
-// 		deviceID = "792ac520-0733-4ffe-8137-8aba3ca446d7"
-// 	)
-// 	// Run the test
-// 	for _, params := range testParams {
-// 		// Create app under test
-// 		app, mockIoT, mockEmail := getStubbedApp(t)
-// 		// Configure lookup to pass
-// 		device := iot.Device{
-// 			Name:      "1",
-// 			DeviceId:  deviceID,
-// 			AccountId: "af6f3796-c446-4850-9fda-65936cab9b6d",
-// 		}
-// 		mockIoT.EXPECT().GetThing(gomock.Eq(deviceID)).Return(&device, nil)
-// 		// Configure call to update visibility state
-// 		mockIoT.EXPECT().SetVisibiltyState(gomock.Eq(device.DeviceId), gomock.Eq(params.status))
-// 		// Configure call to send emails
-// 		mockEmail.EXPECT().SendVisibilityStatus(gomock.Eq(&device), gomock.Eq(params.time), gomock.Eq(params.status))
-// 		// Prepare an event
-// 		event := DeviceLifecycleEvent{DeviceID: deviceID, EventType: params.event, Timestamp: params.timestamp}
-// 		// Run the test
-// 		assert.Nil(t, app.RunJob(nil, event))
-// 	}
-// }
+func TestEmailsSent(t *testing.T) {
+	// Prepare some test parameters
+	const (
+		deviceID = "b6d62b30-00ac-49c4-9268-88559a46889f"
+		accountID = "c6d62b30-00ac-49c4-9268-88559a46889f"
+		eventTimeStr = "2020-12-12T19:58:16+00:00"
+	)
+	// Create app under test
+	app, mockIoT, mockShadow, mockUpdater := getStubbedApp(t)
+	// Expect a call to shadow
+	mockShadow.EXPECT().GetConnectionStatus(deviceID).Return(
+		// Indicate the status hasn't been updated for a while
+		&shadow.ConnectionState{State: true, Updated: createTime(t, "2020/12/12 00:00:00")},
+		nil,
+	)
+	// Configure lookup to succeed
+	device := iot.Device{
+		AccountId: accountID,
+		DeviceId: deviceID,
+		Name: "Alderney",
+	}
+	mockIoT.EXPECT().GetThing(deviceID).Return(
+		&device,
+		nil,
+	)
+	// Expect a call to update status
+	eventTime, err := time.Parse(time.RFC3339, eventTimeStr);
+	assert.Nil(t, err)
+	mockUpdater.EXPECT().UpdateConnectionStatus(&device, eventTime, false)
+	// Run test
+	// Prepare an event
+	event := events.SQSEvent{
+		Records: []events.SQSMessage{
+			{Body: fmt.Sprintf(`{"deviceId":"%s","time":"%s"}`, deviceID, eventTimeStr)},
+		},
+	}
+	assert.Nil(t, app.Handler(nil, event))
+}
 
 func getStubbedApp(t *testing.T) (*app, *MockIoTClient, *MockShadowClient, *MockConnectionUpdater) {
 	// Create mock controller
