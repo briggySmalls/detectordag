ce602725625ffb5fe1ff17a79b21b979aea69fd9
briggySmalls <sam.briggs@entia.co>
2020-12-15 04:22:46 +0000

    Tidy up more

--- edge/tasks.py
+++ edge/tasks.py
@@ -4,11 +4,10 @@ Tasks for maintaining the project.
 Execute 'invoke --list' for guidance on using Invoke
 """
 import platform
-import shutil
 import webbrowser
 from pathlib import Path
 
-from invoke import task
+from invoke import task  # pylint: disable=no-name-in-module
 
 ROOT_DIR = Path(__file__).parent
 SETUP_FILE = ROOT_DIR.joinpath("setup.py")
@@ -24,28 +23,28 @@ DOCS_INDEX = DOCS_BUILD_DIR.joinpath("index.html")
 PYTHON_DIRS = [str(d) for d in [SOURCE_DIR, TEST_DIR]]
 
 
-def _run(c, command, *args, **kwargs):
+def _run(ctx, command, *args, **kwargs):
     """Helper function for running commands"""
     pty = platform.system() != "Windows"
-    c.run(command, *args, pty=pty, **kwargs)
+    ctx.run(command, *args, pty=pty, **kwargs)
 
 
-def _format_dirs_string(c):
+def _format_dirs_string(ctx):
     """Get the directories/files to format"""
-    return " ".join((str(f) for f in (*c.PYTHON_FILES, *c.PYTHON_DIRS)))
+    return " ".join((str(f) for f in (*ctx.PYTHON_FILES, *ctx.PYTHON_DIRS)))
 
 
 @task(
     help=dict(check="Checks if source is formatted without applying changes"),
 )
-def format_black(c, check=False):
+def format_black(ctx, check=False):
     """
     Format code
     """
     black_options = "--line-length 79"
     if check:
         black_options += " --check --diff"
-    _run(c, f"black {black_options} {_format_dirs_string(c)}")
+    _run(ctx, f"black {black_options} {_format_dirs_string(ctx)}")
 
 
 @task(
@@ -53,7 +52,7 @@ def format_black(c, check=False):
         check="Checks if imports are formatted without applying changes"
     ),
 )
-def format_isort(c, check=False):
+def format_isort(ctx, check=False):
     """
     Format imports
     """
@@ -62,91 +61,72 @@ def format_isort(c, check=False):
     if check:
         isort_options += " --check-only --diff"
     # Run the ting
-    _run(c, f"isort {isort_options} {_format_dirs_string(c)}")
+    _run(ctx, f"isort {isort_options} {_format_dirs_string(ctx)}")
 
 
 @task(
     help=dict(check="Checks if source is formatted without applying changes"),
-)
-def format(c, check=False):
+)  # pylint: disable=redefined-builtin
+def format(ctx, check=False):
     """
     Do some clever formatting
     """
     # Format code using black
-    format_black(c, check)
+    format_black(ctx, check)
     # Run isort
-    format_isort(c, check)
+    format_isort(ctx, check)
 
 
 @task
-def lint_flake8(c):
+def lint_flake8(ctx):
     """
     Lint code with flake8
     """
-    _run(c, "flake8 {}".format(" ".join(PYTHON_DIRS)))
+    _run(ctx, "flake8 {}".format(_format_dirs_string(ctx)))
 
 
 @task
-def lint_pylint(c):
+def lint_pylint(ctx):
     """
     Lint code with pylint
     """
-    _run(c, "pylint {}".format(" ".join(PYTHON_DIRS)))
+    _run(ctx, "pylint {}".format(_format_dirs_string(ctx)))
 
 
 @task
-def lint_mypy(c):
+def lint_mypy(ctx):
     """
     Lint code with mypy
     """
-    _run(c, "mypy --strict --allow-untyped-decorators")
+    _run(ctx, "mypy --strict --allow-untyped-decorators")
 
 
 @task(lint_flake8, lint_pylint, lint_mypy)
-def lint(c):
+def lint(_):
     """
     Run all linting
     """
-    pass
 
 
 @task
-def test(c):
+def test(ctx):
     """
     Run tests
     """
-    pty = platform.system() != "Windows"
-    _run(c, "python {} test".format(SETUP_FILE), pty=pty)
+    _run(ctx, "python {} test".format(SETUP_FILE))
 
 
 @task(help={"publish": "Publish the result via coveralls"})
-def coverage(c, publish=False):
+def coverage(ctx, publish=False):
     """
     Create coverage report
     """
-    c.run("coverage run --source {} -m pytest".format(SOURCE_DIR))
-    c.run("coverage report")
+    ctx.run("coverage run --source {} -m pytest".format(SOURCE_DIR))
+    ctx.run("coverage report")
     if publish:
         # Publish the results via coveralls
-        c.run("coveralls")
+        ctx.run("coveralls")
     else:
         # Build a local report
-        c.run("coverage html")
+        ctx.run("coverage html")
         webbrowser.open(COVERAGE_REPORT.as_uri())
-
-
-@task
-def docs(c):
-    """
-    Generate documentation
-    """
-    c.run("sphinx-build -b html {} {}".format(DOCS_DIR, DOCS_BUILD_DIR))
-    webbrowser.open(DOCS_INDEX.as_uri())
-
-
-@task
-def clean_docs(c):
-    """
-    Clean up files from documentation builds
-    """
-    c.run("rm -fr {}".format(DOCS_BUILD_DIR))
