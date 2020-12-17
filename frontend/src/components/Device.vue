3d26faeeaa4b22de9c3c43266327219d4b627123
briggySmalls <sam.briggs@entia.co>
2020-12-17 16:26:11 +0000

    Start working on nice display

--- frontend/src/components/Device.vue
+++ frontend/src/components/Device.vue
@@ -1,13 +1,23 @@
 <template>
   <b-card :title="device.name">
+    <div class="status-graphic d-inline-block">
+      <div class="power-icon-container" v-bind:class="[deviceStateClass]">
+        <img v-if="powerState === powerStateEnum.Off"
+          alt="Power off" src="../assets/power-off.svg" class="power-icon">
+        <img v-else-if="powerState === powerStateEnum.On"
+          alt="Power on" src="../assets/power-on.svg" class="power-icon">
+      </div>
+      <img v-if="connectionState === connectedStateEnum.Disconnected"
+        alt="Disconnected" src="../assets/no-signal.svg" class="connection-icon">
+    </div>
     <b-card-body>
-      <b-card-sub-title class="mb-2">{{ deviceStatus }}</b-card-sub-title>
+      <b-card-sub-title class="mb-2">{{ deviceStateText }}</b-card-sub-title>
     </b-card-body>
     <b-list-group flush>
-      <b-list-group-item v-if="isOff">
+      <b-list-group-item v-if="powerState === powerStateEnum.Off">
         Turned off at: {{ device.state.updated.toLocaleString() }}
       </b-list-group-item>
-      <b-list-group-item v-if="isDisconnected">
+      <b-list-group-item v-if="connectionState === connectedStateEnum.Disconnected">
         Last seen at: {{ device.connection.updated.toLocaleString() }}
       </b-list-group-item>
     </b-list-group>
@@ -35,9 +45,28 @@ enum ConnectedState {
   Disconnected,
 }
 
+interface StateData {
+  class: string;
+  text: string;
+}
+
 @Component
 export default class Device extends Vue {
+  // Declare some enums so we can use them in the template
+  private const deviceStateEnum: typeof DeviceState = DeviceState
+
+  private const powerStateEnum: typeof PowerState = PowerState
+
+  private const connectedStateEnum: typeof ConnectedState = ConnectedState
+
   @Prop() private device!: DeviceModel;
+
+  private const deviceStateInfo: Record<DeviceState, StateData> = {
+    [DeviceState.On]: { class: 'on', text: 'On' },
+    [DeviceState.Off]: { class: 'off', text: 'Off' },
+    [DeviceState.WasOn]: { class: 'was-on', text: 'Was On' },
+    [DeviceState.WasOff]: { class: 'was-off', text: 'Was Off' },
+  };
 
   private get deviceStatus(): string {
     switch (this.state) {
@@ -54,19 +83,19 @@ export default class Device extends Vue {
     }
   }
 
-  private get state(): DeviceState {
+  private get deviceState(): DeviceState {
     if (this.connectionStatue === ConnectedState.Connected) {
       return (this.powerState === PowerState.On) ? DeviceState.On : DeviceState.Off;
     }
     return (this.powerState === PowerState.On) ? DeviceState.WasOn : DeviceState.WasOff;
   }
 
-  private get isOff(): bool {
-    return this.powerState === PowerState.Off;
+  private get deviceStateClass(): string {
+    return this.deviceStateInfo[this.deviceState].class;
   }
 
-  private get isDisconnected(): bool {
-    return this.connectionState === ConnectedState.Disconnected;
+  private get deviceStateText(): string {
+    return this.deviceStateInfo[this.deviceState].text;
   }
 
   private get connectionState(): ConnectedState {
@@ -92,3 +121,43 @@ export default class Device extends Vue {
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+@import "node_modules/bootstrap/scss/bootstrap";
+
+.status-graphic {
+  position: relative;
+  padding: 1em;
+
+  .power-icon-container {
+    border-radius: 50%;
+    width: 10em;
+    height: 10em;
+
+    &.on, &.was-on {
+      background-color: theme-color("success");
+    }
+    &.off, &.was-off {
+      background-color: theme-color("danger");
+    }
+  }
+
+  img.power-icon {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  img.connection-icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding: 0.8em;
+    width: 3em;
+    height: 3em;
+    background-color: theme-color("warning");
+    border-radius: 1em;
+  }
+}
+</style>
