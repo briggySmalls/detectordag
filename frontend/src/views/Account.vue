5798cc96b7d12387c5341f431e436c63c6db5940
briggySmalls <sam.briggs@entia.co>
2020-12-18 04:02:00 +0000

    Swap for a list

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -17,33 +17,20 @@
                 <b-button @click="addTag()" variant="primary">Add</b-button>
               </b-input-group-append>
             </b-input-group>
-            <ul
-              id="my-custom-tags-list"
-              class="list-unstyled d-inline-flex flex-wrap mb-0"
-              aria-live="polite"
-              aria-atomic="false"
-              aria-relevant="additions removals"
-            >
-              <!-- Always use the tag value as the :key, not the index! -->
-              <!-- Otherwise screen readers will not read the tag
-                  additions and removals correctly -->
-              <b-card
+            <b-list-group>
+              <b-list-group-item
                 v-for="tag in tags"
                 :key="tag"
-                :id="`my-custom-tags-tag_${tag.replace(/\s/g, '_')}_`"
-                tag="li"
-                class="mt-1 mr-1"
-                body-class="py-1 pr-2 text-nowrap"
               >
-                <strong>{{ tag }}</strong>
+                {{ tag }}
                 <b-button
                   @click="removeTag(tag)"
                   variant="link"
                   size="sm"
                   :aria-controls="`my-custom-tags-tag_${tag.replace(/\s/g, '_')}_`"
                 >remove</b-button>
-              </b-card>
-            </ul>
+              </b-list-group-item>
+            </b-list-group>
           </template>
         </b-form-tags>
       </b-form-group>
