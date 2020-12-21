582ab9b5ec1e7d3a55294b1f938ce81efdc18f01
briggySmalls <briggySmalls90@gmail.com>
2020-12-21 18:21:35 +0000

    Add a cute footer!

--- frontend/src/layouts/Topbar.vue
+++ frontend/src/layouts/Topbar.vue
@@ -2,7 +2,7 @@
   <div id="topbar">
     <!-- Display a navbar when logged in -->
     <NavbarComponent />
-    <b-container id= "content-container" fluid="sm">
+    <b-container id="content-container" fluid="sm">
       <!-- Title -->
       <slot name="header">
         <h1 class="mt-5">{{ title }}</h1>
@@ -12,6 +12,9 @@
       <!-- Error -->
       <ErrorComponent :error="error" />
     </b-container>
+    <footer id="footer">
+      Made with ❤ by <a href="https://sambriggs.dev">sam briggs</a>
+    </footer>
   </div>
 </template>
 
@@ -35,6 +38,25 @@ export default class Topbar extends Vue {
 
 <style lang="scss" scoped>
 @import "../app.scss";
+
+$footer-padding: 100px;
+
+#topbar {
+  min-height: 100vh;
+  position: relative;
+
+  #content-container {
+    padding-bottom: $footer-padding;
+  }
+
+  footer#footer {
+    // Make the footer sit at the bottom of the page
+    margin-top: 5em;
+    position: absolute;
+    width:100%;
+    bottom: 0;
+  }
+}
 
 @include media-breakpoint-up(sm) {
   #content-container {
