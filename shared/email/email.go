3d346d4c9bd4fcd0246246da3a005dcc69eff18a
briggySmalls <briggySmalls90@gmail.com>
2020-12-28 22:40:58 +0000

    Use assets in emails

--- shared/email/email.go
+++ shared/email/email.go
@@ -86,22 +86,22 @@ var stateDataLookup = map[StateType]stateData{
 	StateTypeOn: {
 		Title:       "On",
 		Description: "The power is on!",
-		ImageSrc:    "https://detectordag.tk/android-chrome-192x192.png",
+		ImageSrc:    "https://detectordag.tk/on.png",
 	},
 	StateTypeOff: {
 		Title:       "Off",
 		Description: "Your dag says that the power is off",
-		ImageSrc:    "https://detectordag.tk/android-chrome-192x192.png",
+		ImageSrc:    "https://detectordag.tk/off.png",
 	},
 	StateTypeWasOn: {
 		Title:       "Was On",
 		Description: "We've lost contact with your dag. The power was on the last we heard...",
-		ImageSrc:    "https://detectordag.tk/android-chrome-192x192.png",
+		ImageSrc:    "https://detectordag.tk/on-disconnected.png",
 	},
 	StateTypeWasOff: {
 		Title:       "Was Off",
 		Description: "Your dag noticed the power go, and then we lost contact. It may have run out of battery.",
-		ImageSrc:    "https://detectordag.tk/android-chrome-192x192.png",
+		ImageSrc:    "https://detectordag.tk/off-disconnected.png",
 	},
 }
 
