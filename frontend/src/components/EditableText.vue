4f39e25860d132e32d4cceffa26cad2505d19455
briggySmalls <briggySmalls90@gmail.com>
2021-01-02 23:45:09 +0000

    Fix bug that didn't send input value

--- frontend/src/components/EditableText.vue
+++ frontend/src/components/EditableText.vue
@@ -12,12 +12,14 @@
         >Device name</label>
         <b-form-input
           id="inline-form-input-name"
+          v-model="input"
           class="mb-2 mr-sm-2 mb-sm-0"
           type="text"
           :placeholder="value"
         />
         <b-input-group-append>
           <b-button
+            type="submit"
             variant="primary"
             @click="submit"
           >
@@ -27,8 +29,15 @@
       </b-input-group>
     </b-form>
     <template v-else>
-      <div class="d-inline-block"><slot>{{ value }}</slot></div>
-      <div class="d-inline-block"><b-icon-pencil-square class="edit-icon" @click="edit()" /></div>
+      <div class="d-inline-block">
+        <slot>{{ value }}</slot>
+      </div>
+      <div class="d-inline-block">
+        <b-icon-pencil-square
+          class="edit-icon"
+          @click="edit()"
+        />
+      </div>
     </template>
   </div>
 </template>
@@ -42,6 +51,8 @@ export default class EditableText extends Vue {
 
   @Prop() private value!: string;
 
+  private input = '';
+
   private edit() {
     if (this.isEditing) {
       return;
@@ -53,7 +64,8 @@ export default class EditableText extends Vue {
   private submit() {
     this.$logger.debug('submitted');
     this.isEditing = false;
-    this.$emit('edited', this.value);
+    // Get the current value
+    this.$emit('edited', this.input);
   }
 }
 </script>
